8e92f7fee6a6e8838b2eabbcb205c2664176477c
Arnold Balliu <arnold.balliu@gmail.com>
2017-04-10 16:48:56 -0400

    added Podfile

--- BuzzBand/BBPairController.swift
+++ BuzzBand/BBPairController.swift
@@ -16,7 +16,6 @@ class BBPairController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'BuzzBand' do
+  use_frameworks!
+  pod 'Bean-iOS-OSX-SDK'
+end
