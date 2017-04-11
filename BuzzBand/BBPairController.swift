52da4b96b7e27f70cc3b601d9a3c8bbeee3d6bac
Arnold Balliu <arnold.balliu@gmail.com>
2017-04-10 23:16:59 -0400

    attempting to show BuzzBand device on screen

--- BuzzBand/BBPairController.swift
+++ BuzzBand/BBPairController.swift
@@ -8,19 +8,85 @@
 
 import UIKit
 
-class BBPairController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate {
+class BBPairController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate , UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var nearbyDevicesTable: UITableView!
     
+    var beanManager: PTDBeanManager?
+    var myBean: PTDBean?
+    var devicesArray = [PTDBean]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.nearbyDevicesTable.delegate = self
+        
+        self.beanManager = PTDBeanManager()
+        self.beanManager!.delegate = self
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // BEAN STUFF
+    
+    func beanManagerDidUpdateState(_ beanManager: PTDBeanManager!) {
+    
+        if beanManager!.state == BeanManagerState.poweredOn {
+            startScanning()
+        }
+    }
+    
+    func startScanning(){
+        var error: NSError?
+        self.beanManager!.startScanning(forBeans_error: &error)
+        if let e = error {
+            print(e)
+        }
+    }
+    
+    func beanManager(_ beanManager: PTDBeanManager!, didDiscover bean: PTDBean!, error: Error!) {
+        
+        if let e = error {
+            print(e)
+        }
+        
+        self.devicesArray.append(bean)
+        self.nearbyDevicesTable.reloadData()
+    }
+    
+    func connectToBean(bean: PTDBean!) {
+        var error : NSError?
+        self.beanManager?.connect(to: bean, withOptions:nil, error: &error)
+        bean.delegate = self
+    }
+    
+    
+    // TABLE VIEW STUFF
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (self.devicesArray.count)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("hi")
+        let cell = self.nearbyDevicesTable.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath)
+        cell.textLabel?.text = self.devicesArray[indexPath.row].name
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.textAlignment = NSTextAlignment.center
+        cell.selectionStyle = UITableViewCellSelectionStyle.none
+        return cell
     }
 
 }
